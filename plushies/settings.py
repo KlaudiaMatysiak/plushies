59ed2ea9de09dc3c9813b5181463facba78c6595
KlaudiaMatysiak <klatysiak@gmail.com>
2022-10-26 21:17:21 +0000

    Remove secret key

--- plushies/settings.py
+++ plushies/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xq8^_s2q5971p7!ojnpn5pz=c_7g7!v@y*sj2-wgfsp-s*j4^2'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
