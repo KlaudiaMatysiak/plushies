eb410ee17dc3f3e1f8c89856e4fd9b86f8dc16c9
KlaudiaMatysiak <klatysiak@gmail.com>
2022-10-27 21:42:07 +0000

    Register models in admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Product, Category, Size
+
 
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'size',
+        'color',
+        'price',
+        'image',
+    )
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class SizeAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Size, SizeAdmin)
