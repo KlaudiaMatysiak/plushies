53c5b95e3bf49fe5f5f5f2b7b784cb6aaa3d4469
KlaudiaMatysiak <klatysiak@gmail.com>
2022-10-27 21:45:09 +0000

    Create view for the shop with products

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+def shop(request):
+    """ A view to show all the products in the shop"""
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/shop.html', context)
+
+
