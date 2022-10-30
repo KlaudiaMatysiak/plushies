b26e05053e6b5d386dfa1ac14e918190b5c6aa5d
KlaudiaMatysiak <klatysiak@gmail.com>
2022-10-30 23:21:35 +0000

    Add view for modifing quantity product

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 
 def view_cart(request):
@@ -22,3 +22,22 @@ def add_to_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect('view_cart')
+
+
+def modify_cart(request, item_id):
+    """
+    A view of modifying a quantity of the selected product
+    to the specified amount
+    """
+
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[item_id] = quantity
+    else:
+        cart.pop(item_id)
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
