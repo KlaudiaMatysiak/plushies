fe656555b45eda418c91dc3010a6f6952393e89e
KlaudiaMatysiak <klatysiak@gmail.com>
2022-10-30 23:07:17 +0000

    Add view for adding quantity product to the cart

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_cart(request):
     """ A view of the cart contents """
     return render(request, 'cart/cart.html')
+
+
+def add_to_cart(request, item_id):
+    """
+    A view of adding a quantity of the selected product
+    to the shopping cart
+    """
+
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
+
+    request.session['cart'] = cart
+    return redirect('view_cart')
