8a5bbabf67627c519bee23325efdd5ff5be49200
KlaudiaMatysiak <klatysiak@gmail.com>
2022-11-01 21:47:22 +0000

    Add success messages for modifying and removing products

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import (
+    render, redirect, reverse, HttpResponse, get_object_or_404)
 from django.contrib import messages
 from products.models import Product
 
@@ -13,7 +14,7 @@ def add_to_cart(request, item_id):
     A view of adding a quantity of the selected product
     to the shopping cart
     """
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
@@ -41,13 +42,21 @@ def modify_cart(request, item_id):
     to the specified amount
     """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
 
     if quantity > 0:
         cart[item_id] = quantity
+        messages.success(
+            request,
+            f'Changed {product.name} size {product.size} quantity\
+                to {quantity} in to your cart')
     else:
         cart.pop(item_id)
+        messages.success(
+            request,
+            f'Removed {product.name} size {product.size} from your cart')
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
@@ -57,12 +66,19 @@ def remove_from_cart(request, item_id):
     """ A view of removing product from cart """
 
     try:
+        product = get_object_or_404(Product, pk=item_id)
         cart = request.session.get('cart', {})
 
         cart.pop(item_id)
+        messages.success(
+            request,
+            f'Removed {product.name} size {product.size} from your cart')
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(
+            request,
+            f'Error emoving item: {e}')
         return HttpResponse(status=500)
