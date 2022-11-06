4afa78f4d74beb32efec526271d6adf080102b4d
KlaudiaMatysiak <klatysiak@gmail.com>
2022-11-06 18:39:11 +0000

    Add loading overlay

--- checkout/static/checkout/js/checkout.js
+++ checkout/static/checkout/js/checkout.js
@@ -46,6 +46,8 @@ form.addEventListener('submit', function(ev) {
     ev.preventDefault();
     card.update({ 'disabled': true});
     $('#submit-button').attr('disabled', true);
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
@@ -55,6 +57,8 @@ form.addEventListener('submit', function(ev) {
             let errorDiv = document.getElementById('card-errors');
             let html = `<i class="icon fa-solid fa-xmark"></i> ${result.error.message}`;
             $(errorDiv).html(html);
+            $('#payment-form').fadeToggle(100);
+            $('#loading-overlay').fadeToggle(100);
             card.update({ 'disabled': false});
             $('#submit-button').attr('disabled', false);
         } else {
