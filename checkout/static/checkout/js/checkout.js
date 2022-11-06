2f8b786f534140fd9b9dca49a2975a24beb36a58
KlaudiaMatysiak <klatysiak@gmail.com>
2022-11-06 16:59:16 +0000

    Create stripe element

---
+++ checkout/static/checkout/js/checkout.js
@@ -0,0 +1,29 @@
+/**
+* Core logic/payment flow comes from:
+* https://stripe.com/docs/payments/accept-a-payment
+*
+* CSS from here:
+* https://stripe.com/docs/stripe-js
+*/
+
+let stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+let clientSecret = $('#id_client_secret').text().slice(1, -1);
+const stripe = Stripe(stripePublicKey);
+
+let elements = stripe.elements();
+let style = {
+    base: {
+        color: '#000',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
+};
+let card = elements.create('card', {style: style});
+card.mount('#card-element');
