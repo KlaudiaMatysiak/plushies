fc279f16a0f98e96580431fb15114e2ee6ad2e16
KlaudiaMatysiak <klatysiak@gmail.com>
2022-11-09 09:12:34 +0000

    Remove print statement

--- checkout/views.py
+++ checkout/views.py
@@ -19,7 +19,6 @@ import json
 def cache_checkout_data(request):
     try:
         pid = request.POST.get('client_secret').split('_secret')[0]
-        print('pid', pid)
         stripe.api_key = settings.STRIPE_SECRET_KEY
         stripe.PaymentIntent.modify(pid, metadata={
             'cart': json.dumps(request.session.get('cart', {})),
