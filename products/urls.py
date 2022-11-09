49aed5f430ce77a172a237024ffdc82b6a8ea47a
KlaudiaMatysiak <klatysiak@gmail.com>
2022-11-09 20:27:03 +0000

    Add url for editing the product

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.shop, name='shop'),
     path('<int:product_id>/', views.show_product, name='show_product'),
     path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
 ]
