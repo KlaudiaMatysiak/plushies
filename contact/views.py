2ffbbc3d3114562c14764743679845c4202a7ff0
KlaudiaMatysiak <klatysiak@gmail.com>
2022-11-11 10:37:52 +0000

    Add view for contact form

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from django.contrib import messages
+from .forms import ContactForm
 
 
 def contact(request):
     """ A view of the Contact Page """
-    return render(request, 'contact/contact.html')
+    contact_form = ContactForm(request.POST or None)
+    if request.method == 'POST':
+        if contact_form.is_valid():
+            contact_form.save()
+            messages.success(request, 'Thanks for your message!')
+    context = {
+        'form': contact_form,
+    }
+    return render(request, 'contact/contact.html', context)
