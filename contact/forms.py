7a189d69e925505a22be4af0e64c4214489b03d5
KlaudiaMatysiak <klatysiak@gmail.com>
2022-11-11 10:53:17 +0000

    Add placeholders, remove labels and set autofocus for the contact form

--- contact/forms.py
+++ contact/forms.py
@@ -9,3 +9,21 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders, remove auto generated labels,
+        set autofocus on full name field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'name': 'Name',
+            'email': 'Email Address',
+            'message': 'Message',
+        }
+
+        self.fields['name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].label = False
